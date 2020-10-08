000f0b23dfabf5987dd58cfde82fb5d6e1e5b385
Dong Py Tran <67514451+dongpytran@users.noreply.github.com>
2020-10-09 01:08:42 +0700

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -65,7 +65,7 @@ class Contact extends React.Component{
             <div className="container">
                 <div className="row">
                         <div className="col-lg-8">
-                            <form id="contact-form" name="contact-form" data-netlify="true" onSubmit={this.handleSubmit}>
+                            <form id="contact-form" data-netlify="true" onSubmit={this.handleSubmit}>
                 <ul>
               <li className="wow fadeInUp" data-wow-delay="1.4s">
                 <label>
